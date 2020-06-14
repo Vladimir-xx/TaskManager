2fcd4e508fbaef17281a150477f6fe7f68ddb2b7
Vladimir-xx <vladimirstrong6@gmail.com>
2020-06-14 21:50:25 +0300

    Сорян забыл создать ветку .. Пушу так . Расскидал базу по проекту.. сделал валидацию проекта по логину нЕМНОГО РОФИКСИЛ...С ДОБАВЛЕНИЕМ пРОЕКТОВ

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ function getData() {
             function name(){
                 if(userTask.login == credentialsInit.name ){
                     console.log('login valid ');
-                    document.querySelector('.project1').innerHTML = userTask.projects[0].name;
+                    document.querySelector('.project2').innerHTML = userTask.projects[0].name;
                     document.querySelector('.nameTask').innerHTML = userTask.projects[0].tasks[0].name;
                     document.querySelector('.textTask').innerHTML = userTask.projects[0].tasks[0].description;
 
@@ -56,5 +56,26 @@ function getData() {
         }
 
     });
+
+}
+//===============================================Add-Project================================================
+let addProject  = document.querySelector('.addplus').onclick = function add() {
+
+            let project = document.querySelector('.projects-container');
+            project.innerHTML += '  <div class="projects-container">\n' +
+                '\n' +
+                '                    <div class="dropdown dropMenu">\n' +
+                '                        <a href="#" class="project2" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"\n' +
+                '                           aria-expanded="false">Project</a>\n' +
+                '                        </a>\n' +
+                '\n' +
+                '                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">\n' +
+                '                            <a class="dropdown-item" href="#">Add Task</a>\n' +
+                '                            <a class="dropdown-item" href="#">Delete Project</a>\n' +
+                '\n' +
+                '                        </div>\n' +
+                '                    </div>'
+
+
 }
 
